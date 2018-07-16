9919192ffff89e5894b0cbbbb27d684594edb7a7
tanmaniac <tanmaybangalore@gmail.com>
2018-07-15 21:38:43 -0400

    Kth to last
    Implement an algorithm to find the kth to last element of a singly linked list.

---
+++ include/linked-lists/KthToLast.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "LinkedList.h"
+
+#include <sstream>
+#include <stdexcept>
+
+namespace linkedlists {
+/**
+ * \brief  Implement an algorithm to find the kth to last element of a singly linked list.
+ *         Time complexity: O(N). Space complexity: O(1).
+ *
+ * \param list  Input list
+ * \param k     kth-to-last element to retreive
+ * \return kth-to-last value of the input list.
+ */
+template <class T>
+T kthToLast(ll::ForwardList<T>& list, const size_t k) {
+    // Base case
+    auto head = list.getHead();
+    if (head == nullptr) {
+        throw std::length_error("k exceeds length of input list - list is empty");
+    }
+
+    // Iterate over list. First we'll iterate k times to get a pointer to the kth value
+    auto kthNode = list.getHead();
+    // Only go to the last valid index at most
+    size_t count = 0;
+    while (kthNode->_next != nullptr && count < k - 1) {
+        count++;
+        kthNode = kthNode->_next;
+    }
+
+    // Make sure we actually have the kth value
+    if (count != k - 1) {
+        std::stringstream err;
+        err << "k exceeds length of input list: k = " << k << " but max index = " << count;
+        throw std::length_error(err.str());
+    }
+
+    // Move both the head and the kthNode pointer until kthNode is at the end
+    while (kthNode->_next != nullptr) {
+        head = head->_next;
+        kthNode = kthNode->_next;
+    }
+
+    // head points to the kth to last node
+    return head->_value;
+}
+} // namespace linkedlists
--- test/LinkedLists.cpp
+++ test/LinkedLists.cpp
@@ -1,3 +1,4 @@
+#include "include/linked-lists/KthToLast.h"
 #include "include/linked-lists/LinkedList.h"
 #include "include/linked-lists/RemoveDups.h"
 
@@ -41,4 +42,26 @@ TEST(LINKED_LISTS, REMOVE_DUPS) {
         EXPECT_EQ(expectedOut[i], head->_value);
         head = head->_next;
     }
+}
+
+TEST(LINKED_LISTS, KTH_TO_LAST) {
+    std::vector<int> testVec{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
+
+    ll::ForwardList<int> testList;
+
+    // Test for empty lists
+    EXPECT_THROW(linkedlists::kthToLast(testList, 1), std::length_error);
+
+    for (const auto& val : testVec) {
+        testList.appendToTail(val);
+    }
+
+    // Test for k longer than the length of the input list
+    EXPECT_THROW(linkedlists::kthToLast(testList, testVec.size() + 1), std::length_error);
+
+    // Fifth from last is value 6
+    const size_t k = 5;
+    EXPECT_EQ(linkedlists::kthToLast(testList, k), 6);
+
+    EXPECT_EQ(1, 1);
 }
